4a1304b834c0a204c33f8a45b64efba4fab17a32
zhangzhaohui <zhangzhaohui@wjika.com>
2017-12-18 15:22:57 +0800

    待收件按钮的逻辑。

--- app/src/main/java/com/shinaier/laundry/snlfactory/ordermanage/ui/fragment/OrderManageWillTakeOrderFragment.java
+++ app/src/main/java/com/shinaier/laundry/snlfactory/ordermanage/ui/fragment/OrderManageWillTakeOrderFragment.java
@@ -1,6 +1,7 @@
 package com.shinaier.laundry.snlfactory.ordermanage.ui.fragment;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -15,10 +16,12 @@ import com.common.widget.PullToRefreshBase;
 import com.shinaier.laundry.snlfactory.R;
 import com.shinaier.laundry.snlfactory.base.fragment.BaseFragment;
 import com.shinaier.laundry.snlfactory.main.UserCenter;
+import com.shinaier.laundry.snlfactory.manage.ui.activity.OrderDetailActivity;
 import com.shinaier.laundry.snlfactory.network.Constants;
 import com.shinaier.laundry.snlfactory.network.entity.OrderTakeOrderEntities;
 import com.shinaier.laundry.snlfactory.network.parser.Parsers;
 import com.shinaier.laundry.snlfactory.ordermanage.adapter.CategoryTakeOrderAdapter;
+import com.shinaier.laundry.snlfactory.ordermanage.ui.activity.CancelOrderActivity;
 
 import java.util.IdentityHashMap;
 
@@ -33,6 +36,7 @@ public class OrderManageWillTakeOrderFragment extends BaseFragment implements Vi
     private Context context;
     private FootLoadingListView orderManageWillTakeOrderList;
     private CategoryTakeOrderAdapter categoryTakeOrderAdapter;
+    private OrderTakeOrderEntities orderTakeOrderEntities;
 
     @Nullable
     @Override
@@ -86,10 +90,10 @@ public class OrderManageWillTakeOrderFragment extends BaseFragment implements Vi
             case REQUEST_CODE_WILL_TAKE_ORDER:
                 if (data != null){
                     orderManageWillTakeOrderList.onRefreshComplete();
-                    OrderTakeOrderEntities orderTakeOrderEntities = Parsers.getOrderTakeOrderEntities(data);
+                    orderTakeOrderEntities = Parsers.getOrderTakeOrderEntities(data);
                     if (orderTakeOrderEntities != null){
                         if (orderTakeOrderEntities.getCode() == 0){
-                            if (orderTakeOrderEntities .getResults() != null && orderTakeOrderEntities.getResults().size() > 0){
+                            if (orderTakeOrderEntities.getResults() != null && orderTakeOrderEntities.getResults().size() > 0){
                                 setLoadingStatus(LoadingStatus.GONE);
                                 orderManageWillTakeOrderList.setVisibility(View.VISIBLE);
                                 categoryTakeOrderAdapter = new CategoryTakeOrderAdapter(context, orderTakeOrderEntities.getResults());
@@ -97,27 +101,32 @@ public class OrderManageWillTakeOrderFragment extends BaseFragment implements Vi
                                 categoryTakeOrderAdapter.setPositionListener(new CategoryTakeOrderAdapter.PositionListener() {
                                     @Override
                                     public void cancelOnClick(int position) {
-
+                                        Intent intent = new Intent(context,CancelOrderActivity.class);
+                                        intent.putExtra("id", orderTakeOrderEntities.getResults().get(position).getId());
+                                        startActivity(intent);
                                     }
 
                                     @Override
                                     public void confirmOnClick(int position) {
-
+                                        // TODO: 2017/12/18 添加项目
                                     }
 
                                     @Override
                                     public void onTellClick(int position) {
-
+                                        tell(orderTakeOrderEntities.getResults().get(position).getuMobile());
                                     }
 
                                     @Override
                                     public void onGotoDetail(int position) {
-
+                                        // TODO: 2017/12/16 页面有问题
+                                        Intent intent = new Intent(context,OrderDetailActivity.class);
+                                        intent.putExtra("id",orderTakeOrderEntities.getResults().get(position).getId());
+                                        startActivity(intent);
                                     }
                                 });
                             }
                         }else {
-                            ToastUtil.shortShow(context,orderTakeOrderEntities.getMsg());
+                            ToastUtil.shortShow(context, orderTakeOrderEntities.getMsg());
                         }
                     }
                 }
